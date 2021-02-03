1172712155263cb8b4fc3a3c1c470cc067aa8d67
SurajSS007 <surajsuryawanshi220@gmail.com>
2021-02-03 21:46:46 +0530

    added

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: 'dwy0ayc3b',
+    api_key:'321973912954823',
+    api_secret: 'vacpzt1ezOlPBSLoXghla_eKzc8'
+});
+
+module.exports = { cloudinary };
